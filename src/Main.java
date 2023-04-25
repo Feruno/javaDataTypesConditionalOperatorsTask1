4c9d206e417449a3205175669560f2b7af8f5ab2
Александр Бондаренко <nice.alexsandr@mail.ru>
2023-04-25 19:23:37 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 15000;
+        int countRublesForOneBonusMile = 20;
+
+        int res = ticketPrice/countRublesForOneBonusMile;
+
+        System.out.println("За билет стоимостью = " + ticketPrice + " вы получите " + res + " бонусов");
+
+    }
+}
