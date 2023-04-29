06353f5905964e3fc8844aa3be9b67e14a96be24
Александр Бондаренко <nice.alexsandr@mail.ru>
2023-04-29 21:26:20 +0300

    Reformat code

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-
         int ticketPrice = 15000;
         int countRublesForOneBonusMile = 20;
-
-        int res = ticketPrice/countRublesForOneBonusMile;
+        int res = ticketPrice / countRublesForOneBonusMile;
 
         System.out.println("За билет стоимостью = " + ticketPrice + " вы получите " + res + " бонусов");
-
     }
 }
